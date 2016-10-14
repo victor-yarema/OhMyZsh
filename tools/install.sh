f0f01ed994e2323daf32cc33a37bc75f31bac69c
victor-yarema <victor.yarema@gmail.com>
2016-10-15 02:53:57 +0300

    Switched to own repo.

--- tools/install.sh
+++ tools/install.sh
@@ -69,9 +69,9 @@ fi
 ZSH="${ZSH:-$HOME/.oh-my-zsh}"
 
 # Default settings
-REPO=${REPO:-ohmyzsh/ohmyzsh}
+REPO=${REPO:-victor-yarema/OhMyZsh}
 REMOTE=${REMOTE:-https://github.com/${REPO}.git}
-BRANCH=${BRANCH:-master}
+BRANCH=${BRANCH:-main}
 
 # Other options
 CHSH=${CHSH:-yes}
