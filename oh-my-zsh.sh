d62e5c623aa2a53754f3fc8d3dd70302a693f54a
Victor Yarema <victor.yarema@gmail.com>
2023-03-03 23:33:45 +0100

    Fixed unset variable(s) error(s).

--- oh-my-zsh.sh
+++ oh-my-zsh.sh
@@ -145,7 +145,7 @@ else
 fi
 
 # Append zcompdump metadata if missing
-if (( $zcompdump_refresh )) \
+if (( ${zcompdump_refresh:-} )) \
   || ! command grep -q -Fx "$zcompdump_revision" "$ZSH_COMPDUMP" 2>/dev/null; then
   # Use `tee` in case the $ZSH_COMPDUMP filename is invalid, to silence the error
   # See https://github.com/ohmyzsh/ohmyzsh/commit/dd1a7269#commitcomment-39003489
